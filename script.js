49338acc8bbd1b952f775f7c6bc11d7779376e2f
100gbgaming <anandjyadav922@gmail.com>
2026-03-13 22:27:21 +0530

    Add updateProfile function to update user info

---
+++ script.js
@@ -0,0 +1,21 @@
+function updateProfile(){
+
+document.getElementById("name").innerText =
+document.getElementById("nameInput").value;
+
+document.getElementById("followers").innerText =
+document.getElementById("followersInput").value;
+
+document.getElementById("posts").innerText =
+document.getElementById("postsInput").value;
+
+document.getElementById("bio").innerText =
+document.getElementById("bioInput").value;
+
+document.getElementById("views").innerText =
+document.getElementById("viewsInput").value;
+
+document.getElementById("likes").innerText =
+document.getElementById("likesInput").value;
+
+}
