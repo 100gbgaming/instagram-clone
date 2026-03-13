b537dfb38d1586455e31a916eb3c72a0ac223ddb
100gbgaming <anandjyadav922@gmail.com>
2026-03-13 23:09:45 +0530

    Update and rename style.css to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateProfile(){
+
+document.getElementById("username").innerText =
+document.getElementById("usernameInput").value;
+
+document.getElementById("posts").innerText =
+document.getElementById("postsInput").value;
+
+document.getElementById("followers").innerText =
+document.getElementById("followersInput").value;
+
+document.getElementById("following").innerText =
+document.getElementById("followingInput").value;
+
+document.getElementById("bio").innerText =
+document.getElementById("bioInput").value;
+
+}
